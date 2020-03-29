88ccc29313343a7bdcac5b8a70b019aa75434bbe
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-03-29 20:13:54 +0700

    first laravel deployment to heroku

---
+++ app/Http/Controllers/WayangController.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Wayang;
+
+class WayangController extends Controller
+{
+    public function index(){
+        $result = Wayang::all();
+        if($result){
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response()->json($data);
+    }
+
+    public function show($id)
+    {
+        $result = Wayang::where('id', $id)->first();
+
+        if($result){
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response()->json($data);
+    }
+
+    public function create(request $request){
+        $wayang = new Wayang;
+        $wayang->nama = $request->nama;
+        $wayang->image_url = $request->image_url;
+        $wayang->save();
+
+        if($wayang){
+            $data['code'] = 200;
+            $data['result'] = $wayang;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response($data);
+    }
+
+    public function update(request $request, $id){
+        $result = Wayang::find($id);
+        $result->nama = $request->nama;
+        $result->image_url = $request->image_url;
+        $result->save();
+
+        if($result){
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response($data);
+    }
+
+    public function delete($id){
+        $result = Wayang::find($id);
+        $result->delete();
+
+        if($result){
+            $data['code'] = 200;
+            $data['result'] = $result;
+        } else {
+            $data['code'] = 500;
+            $data['result'] = 'Error';
+        }
+        return response($data);
+    }
+}
---
+++ database/migrations/2020_03_29_114419_create_wayangs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWayangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wayangs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('image_url');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wayangs');
+    }
+}
