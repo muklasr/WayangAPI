529927cb1e110d9c02337c9af39660f4aa6d4b9e
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-03-30 17:55:26 +0700

    change landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>WayangAPI</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,18 +81,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    WayangAPI
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://wayangapi.herokuapp.com//docs">Docs</a>
+                    <a href="https://wayangapi.herokuapp.com//wayang">List Wayang</a>
+                    <a href="https://github.com/muklasr/wayangapi">GitHub</a>
                 </div>
             </div>
         </div>
