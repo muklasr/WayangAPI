9a43fefbacc0e03b7ced725b358834078f0a52c5
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-03-30 18:10:59 +0700

    add wherelike

--- app/Http/Controllers/WayangController.php
+++ app/Http/Controllers/WayangController.php
@@ -35,7 +35,7 @@ class WayangController extends Controller
 
     public function search($nama)
     {
-        $result = Wayang::whereLike('nama', $nama)->get();
+        $result = Wayang::where('nama', 'like' , '%'.$nama.'%')->get();
 
         if($result){
             $data['code'] = 200;
