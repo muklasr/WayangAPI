ac9b7edf7f882c8441aa3edb067dcb157f6d21cf
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-03-30 10:16:59 +0700

    setting cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,9 +15,22 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)
-        ->header(‘Access-Control-Allow-Origin’, ‘*’)
-        ->header(‘Access-Control-Allow-Methods’, ‘GET, POST, PUT, DELETE, OPTIONS’)
-        ->header(‘Access-Control-Allow-Headers’, ‘X-Requested-With, Content-Type, X-Token-Auth, Authorization’);
+
+        header("Access-Control-Allow-Origin: *");
+
+        // ALLOW OPTIONS METHOD
+        $headers = [
+            'Access-Control-Allow-Methods'=> 'POST, GET, OPTIONS, PUT, DELETE',
+            'Access-Control-Allow-Headers'=> 'Content-Type, X-Auth-Token, Origin'
+        ];
+        if($request->getMethod() == "OPTIONS") {
+            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+            return Response::make('OK', 200, $headers);
+        }
+
+        $response = $next($request);
+        foreach($headers as $key => $value)
+            $response->header($key, $value);
+        return $response;
     }
 }
