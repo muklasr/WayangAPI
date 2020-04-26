f07832c1ebfa57521cbd281e447ef66f1bb3418a
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-26 21:03:33 +0700

    delete link

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -65,19 +65,6 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
 
             <div class="content">
                 <div class="title m-b-md">
@@ -86,7 +73,6 @@
 
                 <div class="links">
                     <a href="https://wayangapi.herokuapp.com/docs">Docs</a>
-                    <a href="https://wayangapi.herokuapp.com/api/wayang">List Wayang</a>
                     <a href="https://github.com/muklasr/wayangapi">GitHub</a>
                 </div>
             </div>
