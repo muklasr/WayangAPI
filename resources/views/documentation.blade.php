69926621530b262ce281b4927d3122093a0e3b11
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-26 21:24:17 +0700

    delete unused css

--- resources/views/documentation.blade.php
+++ resources/views/documentation.blade.php
@@ -19,40 +19,6 @@
                 height: 100vh;
                 margin: 0;
             }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 40px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
     </head>
     <body>
