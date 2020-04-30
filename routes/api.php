51a7ea37d234d5ea7d17ba699b13cc2a1502fd82
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-30 19:35:17 +0700

    add header

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin: *');
+header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
