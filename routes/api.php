896e365fd3ac0b27944a49164eb188807c565cae
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-30 19:44:56 +0700

    remove header

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,6 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-header('Access-Control-Allow-Origin: *');
-header( 'Access-Control-Allow-Headers: Authorization, Content-Type' );
-header('Access-Control-Allow-Headers: GET, POST, PUT, DELETE, OPTIONS');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
