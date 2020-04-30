5de6d15338e0033e3537fc6fc62a08fe1f37b160
Muklas Rahmanto <muklas.r@mail.ugm.ac.id>
2020-04-30 20:41:17 +0700

    change handle function

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -15,23 +15,30 @@ class Cors
      */
     public function handle($request, Closure $next)
     {
-
-        header("Access-Control-Allow-Origin: *");
-
-        // ALLOW OPTIONS METHOD
-        $headers = [
-            'Access-Control-Allow-Methods'=> '*',
-            'Access-Control-Allow-Credentials'=> 'true',
-            'Access-Control-Allow-Headers'=> 'GET, POST, PUT, DELETE, OPTIONS'
-        ];
-        if($request->getMethod() == "OPTIONS") {
-            // The client-side application can set only headers allowed in Access-Control-Allow-Headers
-            return Response::make('OK', 200, $headers);
-        }
-
-        $response = $next($request);
-        foreach($headers as $key => $value)
-            $response->header($key, $value);
-        return $response;
+        return $next($request)
+            ->header('Access-Control-Allow-Origin', '*')
+            ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
+            ->header('Access-Control-Allow-Headers', 'Content-Type, Authorizations');
     }
+    // public function handle($request, Closure $next)
+    // {
+
+    //     header("Access-Control-Allow-Origin: *");
+
+    //     // ALLOW OPTIONS METHOD
+    //     $headers = [
+    //         'Access-Control-Allow-Methods'=> '*',
+    //         'Access-Control-Allow-Credentials'=> 'true',
+    //         'Access-Control-Allow-Headers'=> 'GET, POST, PUT, DELETE, OPTIONS'
+    //     ];
+    //     if($request->getMethod() == "OPTIONS") {
+    //         // The client-side application can set only headers allowed in Access-Control-Allow-Headers
+    //         return Response::make('OK', 200, $headers);
+    //     }
+
+    //     $response = $next($request);
+    //     foreach($headers as $key => $value)
+    //         $response->header($key, $value);
+    //     return $response;
+    // }
 }
